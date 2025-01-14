57e684147e7db313b79217ecd8a6d9e9b32a6c3f
Le An Nguyen <nguyenlean96@gmail.com>
2025-01-13 23:06:45 -0500

    Added basing index and implemented actix web application

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
-fn main() {
-    println!("Hello, world!");
+use std::{env, io};
+
+use actix_web::{
+    get,
+    middleware::Logger,
+    App, HttpRequest, HttpResponse, HttpServer, Result,
+};
+use dotenvy::dotenv;
+
+#[get("/")]
+async fn index(_req: HttpRequest) -> Result<HttpResponse> {
+    Ok(HttpResponse::Ok()
+        .content_type("text/html")
+        .body(include_str!("../static/index.html")))
+}
+
+#[actix_web::main]
+async fn main() -> io::Result<()> {
+    dotenv().ok();
+
+    env_logger::init_from_env(env_logger::Env::new().default_filter_or("info"));
+    let app_port = env::var("APP_PORT").unwrap_or("8000".to_string());
+
+    log::info!("Starting server at http://localhost:{}", app_port);
+
+    HttpServer::new(move || App::new().wrap(Logger::default()).service(index))
+        .bind((
+            "127.0.0.1",
+            app_port.parse::<u16>().expect("Invalid port number"),
+        ))?
+        .workers(2)
+        .run()
+        .await
 }
